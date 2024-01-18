383581630ba5ef2b003b5256fa9b872ad14dde2e
wanjiaqi <1420537511@qq.com>
2024-01-18 20:10:40 +0800

    更新2.0版本,添加切换角色语音功能

--- index.js
+++ index.js
@@ -19,14 +19,17 @@ axios.interceptors.response.use(function (response) {
 const btn = document.querySelector('button');
 const chatBox = document.querySelector('.chatBox');
 const audio = document.querySelector('audio');
-
+const header = document.querySelector('.header');
+let Enmae;
 async function op() {
    try {
       chatBox.insertAdjacentHTML("beforeend", `<div class="message my_message">
             <p>${test.value}<br></p>
          </div>`)
-      const a = await axios.get("https://api.lolimi.cn/API/AI/bj.php", { params: { msg: test.value } });
-      const b = await axios.get("https://api.lolimi.cn/API/yyhc/y.php", { params: { msg: a.data.data.output, speaker: "派蒙", noisew: "0.3", sdp: "2", Length: "1", noise: "0.6", type: "1" } })
+      let tv = test.value
+      test.value = "";
+      const a = await axios.get("https://api.lolimi.cn/API/AI/bj.php", { params: { msg: tv } });
+      const b = await axios.get("https://api.lolimi.cn/API/yyhc/y.php", { params: { msg: a.data.data.output, speaker: Enmae, noisew: "0.3", sdp: "2", Length: "1", noise: "0.6", type: "1" } })
       audio.src = b.data.music
       audio.play()
       chatBox.insertAdjacentHTML("beforeend", `<div class="message frnd_message">
@@ -45,5 +48,23 @@ btn.addEventListener('click', async function (e) {
 test.addEventListener('keydown', function (e) {
    if (e.key === "Enter" && test.value.trim() !== ""){
       op()
+   }
+})
+
+
+
+
+// 排他切换角色语音
+header.addEventListener('click',function(e){
+   if (e.target.classList.contains('imgText')) {
+      header.querySelector(".show") && header.querySelector(".show").classList.remove("show");
+      e.target.classList.add("show");
+      Enmae = e.target.firstElementChild.firstElementChild.dataset.name
+      chatBox.innerHTML = ""
+   }else if (e.target.classList.contains('cover')) {
+      header.querySelector(".show") && header.querySelector(".show").classList.remove("show");
+      e.target.parentElement.parentElement.classList.add("show");
+      Enmae = e.target.dataset.name
+      chatBox.innerHTML = ""
    }
 })
