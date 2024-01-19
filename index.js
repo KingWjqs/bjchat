32311fda5568a03439efdf50dde2ee2aa66ba619
wanjiaqi <1420537511@qq.com>
2024-01-19 18:11:19 +0800

    优化结构,添加防抖

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
+const btn = document.querySelector('button');
+const chatBox = document.querySelector('.chatBox');
+const audio = document.querySelector('audio');
+const header = document.querySelector('.header');
 const opmusic = axios.create()
+// 页面初始值
+let Enmae = "派蒙";
+// 接收发送的消息
+let tv;
+let timer
+function renderInfo(fx,pinfo,pheader) {
+   tv = pinfo
+   test.value = ""
+   if (fx === "right") {
+      chatBox.insertAdjacentHTML("beforeend", `<div class="message my_message">
+            <p>${pinfo}<br></p>
+            <div class="rightheadimg">
+               <img src="./img/op.png" class="cover">
+            </div>
+      </div>`)
+   }else if (fx === "left") {
+      chatBox.insertAdjacentHTML("beforeend", `<div class="message frnd_message">
+         <div class="leftheadimg">
+            <img src="${pheader}" class="cover">
+         </div>
+         <p>${pinfo}<br></p>
+      </div>`)
+   }
+}
+function debounce(fn, t) {
+   return (function () {
+     // 2.3.4
+     if (timer) clearTimeout(timer)
+     timer = setTimeout(function () {
+       fn()
+     }, t)
+   })()
+ }
 opmusic.interceptors.request.use(function (config) {
    const Qjshow = document.querySelector('.show');
-   chatBox.insertAdjacentHTML("beforeend", `<div class="message frnd_message">
-      <div class="leftheadimg">
-         <img src="${Qjshow.firstElementChild.firstElementChild.src}" class="cover">
-      </div>
-      <p>...<br></p>
-  </div>`)
+   renderInfo("left", "正在输入", Qjshow.firstElementChild.firstElementChild.src)
    chatBox.scrollTop = chatBox.scrollHeight;
    return config;
 }, function (error) {
@@ -21,48 +53,34 @@ opmusic.interceptors.response.use(function (response) {
    return Promise.reject(error);
 });
 
-const btn = document.querySelector('button');
-const chatBox = document.querySelector('.chatBox');
-const audio = document.querySelector('audio');
-const header = document.querySelector('.header');
-
-let Enmae = "派蒙";
 
 // 封装函数,获取消息和语音
 async function op() {
+   console.log(123);
    const Qjshow = document.querySelector('.show');
    try {
-      chatBox.insertAdjacentHTML("beforeend", `<div class="message my_message">
-            <p>${test.value}<br></p>
-            <div class="rightheadimg">
-               <img src="./img/op.png" class="cover">
-            </div>
-         </div>`)
-      let tv = test.value
-      test.value = "";
       const a = await axios.get("https://api.lolimi.cn/API/AI/bj.php", { params: { msg: tv } });
-      const b = await opmusic.get("https://api.lolimi.cn/API/yyhc/y.php", { params: { msg: a.data.data.output, speaker: Enmae, noisew: "0.3", sdp: "2", Length: "1", noise: "0.6", type: "1" } })
+      const newInfo = a.data.data.output.replace(/（.*?）/g,"")
+      console.log(newInfo);
+      const b = await opmusic.get("https://api.lolimi.cn/API/yyhc/y.php", { params: { msg: newInfo, speaker: Enmae, noisew: "0.3", sdp: "1", Length: "1", noise: "0.6", type: "1" } })
       audio.src = b.data.music
       audio.play()
-      chatBox.insertAdjacentHTML("beforeend", `<div class="message frnd_message">
-         <div class="leftheadimg">
-            <img src="${Qjshow.firstElementChild.firstElementChild.src}" class="cover">
-         </div>
-         <p>${a.data.data.output}<br></p>
-      </div>`)
-      test.value = ""
+      renderInfo("left",newInfo,Qjshow.firstElementChild.firstElementChild.src)
       chatBox.scrollTop = chatBox.scrollHeight;
    } catch (error) {
+      renderInfo("left","网络错误,请检查网络",Qjshow.firstElementChild.firstElementChild.src)
       console.log(error);
    }
 }
-btn.addEventListener('click', async function (e) {
-   op()
+btn.addEventListener('click',function (e) {
+   renderInfo("right",test.value)
+   debounce(op, 3000)
 })
 
 test.addEventListener('keydown', function (e) {
    if (e.key === "Enter" && test.value.trim() !== "") {
-      op()
+      renderInfo("right",test.value);
+      debounce(op, 3000);
    }
 })
 
